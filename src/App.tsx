8dd057bf6eb2825dd5c165e88da9342d5475495b
Bryson <brysonmaul@gmail.com>
2023-03-16 13:13:13 -0400

    the

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 const App = () => {
-  return <div className="App">App works</div>;
+  return (
+    <div className="App">
+      return (
+      <div className="App">
+        <Router>
+          {/* <Header /> */}
+          <Routes>
+            {/* <Route path="/" element={<Main />} />
+  <Route path="/gifs/favorites" element={<Favorites />} />
+         <Route path="/gifs/:id" element={<Details />} /> */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </div>
+      );
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Field To Feast</h1>
+    </div>
+  );
+};
+
+export default Header;
