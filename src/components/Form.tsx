d151524b2a45c16a6c0362457f653612b3433242
Bryson <brysonmaul@gmail.com>
2023-03-17 13:17:59 -0400

    form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,54 @@
-
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import "./Form.css";
 
 const Form = () => {
+  const [farm, setFarms] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [rating, setRating] = useState("");
+  const newFarmHandler = (e: FormEvent): void => {
+    e.preventDefault();
+    setName("");
+    setAddress("");
+    setRating("");
+  };
   return (
-    <form className="Form">
+    <form
+      className="Form"
+      onSubmit={(e) => {
+        newFarmHandler(e);
+      }}
+    >
       <label htmlFor="name">Name of Farm</label>
-      <input type="text" name="name" id="name" />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={name}
+        onChange={(e) => {
+          setName(name);
+        }}
+      />
+      <label htmlFor="address">Address</label>
+      <input
+        type="text"
+        name="address"
+        id="address"
+        value={address}
+        onChange={(e) => {
+          setAddress(address);
+        }}
+      />
+      <label htmlFor="rating">Rating</label>
+      <input
+        type="text"
+        name="rating"
+        id="rating"
+        value={rating}
+        onChange={(e) => {
+          setRating(rating);
+        }}
+      />
     </form>
   );
 };
