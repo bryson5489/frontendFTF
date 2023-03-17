46e7684e552f811612017c46a3256f71bf2a90ee
Bryson <brysonmaul@gmail.com>
2023-03-17 09:54:15 -0400

    messed up

--- src/services/FieldToFeastService.ts
+++ src/services/FieldToFeastService.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
 
-const baseURL = "full url";
+const baseURL =
+  "https://maps.googleapis.com/maps/api/place/textsearch/json?query=farms%20in%20Novi%20Michigan";
 const key: string = process.env.REACT_APP_FARM_KEY || "";
 
 export const getAllFarms = async (): Promise<null> => {
-  return (await axios.get(baseURL)).data;
+  return (await axios.get(baseURL, { params: { key } })).data;
 };
 
-export const getFarmById = async (id: string): Promise<null> => {
-  return (
-    await axios.get(baseURL + "/" + encodeURIComponent(id), {
-      params: { api_key: key },
-    })
-  ).data;
-};
+// export const getFarmById = async (id: string): Promise<null> => {
+//   return (
+//     await axios.get(baseURL + "/" + encodeURIComponent(id), {
+//       params: { api_key: key },
+//     })
+//   ).data;
+// };
